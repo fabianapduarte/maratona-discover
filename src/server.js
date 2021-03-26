f6c7f3e4c3ee20323d42394b0997ccd13fc4dee9
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-03-25 23:08:53 -0300

    loading static files (public directory)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const server = express()
 
+// habilitar arquivos estáticos
+server.use(express.static("public"))
+
 server.get('/', (request, response) => {
   return response.sendFile(__dirname + "/views/index.html")
 })
