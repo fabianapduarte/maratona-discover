7b540c6fc461ef6c45ef3a01a49fff5ed823fc08
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-01 00:37:30 -0300

    add route to edit job

--- src/routes.js
+++ src/routes.js
@@ -71,7 +71,7 @@ const Job = {
           ...job,
           remaining,
           status,
-          budget: Profile.data["value-hour"] * job["total-hours"]
+          budget: Job.services.calculateBudget(job, Profile.data["value-hour"])
         }
       })
     
@@ -94,6 +94,47 @@ const Job = {
       })
     
       return res.redirect('/')
+    },
+
+    show(req, res) {
+      const jobId = req.params.id
+
+      const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+      if (!job) {
+        return res.send('Job not found')
+      }
+
+      job.budget = Job.services.calculateBudget(job, Profile.data["value-hour"])
+
+      return res.render(views + "job-edit", { job })
+    },
+
+    update(req, res) {
+      const jobId = req.params.id
+
+      const job = Job.data.find(job => Number(job.id) === Number(jobId))
+
+      if (!job) {
+        return res.send('Job not found')
+      }
+
+      const updatedJob = {
+        ...job,
+        name: req.body.name,
+        "total-hours": req.body["total-hours"],
+        "daily-hours": req.body["daily-hours"]
+      }
+
+      Job.data = Job.data.map(job => {
+        if(Number(job.id) === Number(jobId)) {
+          job = updatedJob
+        }
+
+        return job
+      })
+
+      return res.redirect('/job/' + jobId)
     }
   },
 
@@ -112,7 +153,9 @@ const Job = {
     
       // restam x dias
       return dayDiff
-    }
+    }, 
+
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
   }
 }
 
@@ -121,7 +164,8 @@ routes.get('/', Job.controllers.index)
 routes.get('/job', Job.controllers.create)
 routes.post('/job', Job.controllers.save)
 
-routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
+routes.get('/job/:id', Job.controllers.show)
+routes.post('/job/:id', Job.controllers.update)
 
 routes.get('/profile', Profile.constrollers.index)
 routes.post('/profile', Profile.constrollers.update)
