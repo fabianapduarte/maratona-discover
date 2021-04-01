a3941de82a755da91840ae70093b7beb28081b02
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-01 01:01:19 -0300

    add route to delete job

--- src/routes.js
+++ src/routes.js
@@ -83,7 +83,7 @@ const Job = {
     },
 
     save(req, res) {
-      const lastId = Job.data[Job.data.length - 1]?.id || 1
+      const lastId = Job.data[Job.data.length - 1]?.id || 0
 
       Job.data.push({
         id: lastId + 1,
@@ -135,6 +135,14 @@ const Job = {
       })
 
       return res.redirect('/job/' + jobId)
+    },
+
+    delete(req, res) {
+      const jobId = req.params.id
+
+      Job.data = Job.data.filter(job => Number(job.id) !== Number(jobId))
+
+      return res.redirect('/')
     }
   },
 
@@ -166,6 +174,7 @@ routes.post('/job', Job.controllers.save)
 
 routes.get('/job/:id', Job.controllers.show)
 routes.post('/job/:id', Job.controllers.update)
+routes.post('/job/delete/:id', Job.controllers.delete)
 
 routes.get('/profile', Profile.constrollers.index)
 routes.post('/profile', Profile.constrollers.update)
