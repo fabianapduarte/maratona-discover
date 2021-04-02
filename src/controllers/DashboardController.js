91457550b69d8d76ec2ca598017fea6d5a49979d
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-02 00:39:57 -0300

    add free hours in index page

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -13,12 +13,16 @@ module.exports = {
       total: jobs.length
     }
 
+    let jobTotalHours = 0
+
     const updatedJobs = jobs.map((job) => {
       const remaining = JobUtils.remainingDays(job)
       const status = remaining <= 0 ? 'done' : 'progress'
 
       // somando a quantidade de status
       statusCount[status] += 1
+
+      jobTotalHours = status === 'progress' ? jobTotalHours += Number(job["daily-hours"]) : jobTotalHours
   
       return {
         ...job,
@@ -27,7 +31,12 @@ module.exports = {
         budget: JobUtils.calculateBudget(job, profile["value-hour"])
       }
     })
+
+    // qtd de horas que quero trabalhar por dia (profile)
+    // MENOS
+    // a qtd de horas por dia de cada job em progresso
+    const freeHours = profile["hours-per-day"] - jobTotalHours
   
-    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount })
+    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours})
   }
 }
