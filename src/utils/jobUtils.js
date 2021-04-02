2b78ed402de75db1d99e23c5b15f7adc5abc0a07
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-01 22:08:34 -0300

    creating utility file for 'job'

---
+++ src/utils/jobUtils.js
@@ -0,0 +1,19 @@
+module.exports = {
+  remainingDays(job) {
+    const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed() //retorna string
+  
+    const createdDate = new Date(job.createdAt)
+    const dueDay = createdDate.getDate() + Number(remainingDays)
+    const dueDateInMs = createdDate.setDate(dueDay)
+  
+    const timeDiffInMs = dueDateInMs - Date.now()
+    // transformar milisegundos em dias
+    const dayInMs = 1000 * 60 * 60 * 24
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+  
+    // restam x dias
+    return dayDiff
+  }, 
+
+  calculateBudget: (job, valueHour) => valueHour * job["total-hours"]
+}
