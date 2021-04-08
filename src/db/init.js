c5c7415e3a90d346580513bf727ef024e0ff5e8f
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-07 23:55:03 -0300

    adding async and await

--- src/db/init.js
+++ src/db/init.js
@@ -1,73 +1,76 @@
 const Database = require('./config')
 
-// inicia a conexão
-Database()
+const initDb = {
+  async init() {
+    // inicia a conexão
+    const db = await Database()
 
+    // cria as tabelas
+    await db.exec(`CREATE TABLE profile (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      avatar TEXT,
+      monthly_budget INT,
+      days_per_week INT,
+      hours_per_day INT,
+      vacation_per_year INT,
+      value_hour INT
+    );`)
 
-// cria as tabelas
-Database.exec(`CREATE TABLE profile (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  avatar TEXT,
-  monthly_budget INT,
-  days_per_week INT,
-  hours_per_day INT,
-  vacation_per_year INT,
-  value_hour INT
-);`)
+    await db.exec(`CREATE TABLE jobs (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      daily_hours INT,
+      total_hours INT,
+      created_at DATETIME
+    );`)
 
-Database.exec(`CREATE TABLE jobs (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT,
-  daily_hours INT,
-  total_hours INT,
-  created_at DATETIME
-);`)
+    // inserção de dados
+    await db.run(`INSERT INTO profile (
+      name,
+      avatar,
+      monthly_budget,
+      days_per_week,
+      hours_per_day,
+      vacation_per_year,
+      value_hour
+    ) VALUES (
+      "Fabiana",
+      "https://github.com/fabianapduarte.png",
+      3000,
+      5,
+      5,
+      4,
+      75
+    );`)
 
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES (
+      "Pizzaria Guloso",
+      2,
+      1,
+      1617514376018
+    );`)
 
-// inserção de dados
-Database.run(`INSERT INTO profile (
-  name,
-  avatar,
-  monthly_budget,
-  days_per_week,
-  hours_per_day,
-  vacation_per_year,
-  value_hour
-) VALUES (
-  "Fabiana",
-  "https://github.com/fabianapduarte.png,
-  3000,
-  5,
-  5,
-  4,
-  75
-);`)
+    await db.run(`INSERT INTO jobs (
+      name,
+      daily_hours,
+      total_hours,
+      created_at
+    ) VALUES (
+      "OneTwo Project",
+      3,
+      47,
+      1617514376018
+    );`)
 
-Database.run(`INSERT INTO jobs (
-  name,
-  daily_hours,
-  total_hours,
-  created_at
-) VALUES (
-  "Pizzaria Guloso",
-  2,
-  1,
-  1617514376018
-);`)
+    // fecha a conexão com o bd
+    await db.close()
+  }
+}
 
-Database.run(`INSERT INTO jobs (
-  name,
-  daily_hours,
-  total_hours,
-  created_at
-) VALUES (
-  "OneTwo Project",
-  3,
-  47,
-  1617514376018
-);`)
-
-
-// fecha a conexão com o bd
-Database.close()
+initDb.init()
