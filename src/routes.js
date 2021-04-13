8629aa67dcb5827fab2203d6ebbc7d15d254b66b
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-13 20:47:26 -0300

    creating routes and controller to login and register

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const routes = express.Router()
 const DashboardController = require('./controllers/DashboardController')
 const ProfileController = require('./controllers/ProfileController')
 const JobController = require('./controllers/JobController')
+const AuthController = require('./controllers/AuthController')
 
 routes.get('/', DashboardController.index)
 
@@ -16,5 +17,8 @@ routes.post('/job/delete/:id', JobController.delete)
 
 routes.get('/profile', ProfileController.index)
 routes.post('/profile', ProfileController.update)
+
+routes.get('/login', AuthController.login)
+routes.get('/register', AuthController.register)
 
 module.exports = routes
