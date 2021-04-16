63587d7d061751dddd8cd970ef9cee841e38c454
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-04-16 00:04:50 -0300

    adding register method to create new users

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,9 +1,30 @@
+const User = require('../model/User')
+
 module.exports = {
-  login(req, res) {
+  signIn(req, res) {
     res.render("login")
   },
 
-  register(req, res) {
+  authenticate(req, res) {
+    
+  },
+
+  signUp(req, res) {
     res.render("register")
+  },
+
+  async register(req, res) {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      avatar: req.body.avatar,
+      "monthly_budget": req.body["monthly-budget"],
+      "days_per_week": req.body["days-per-week"],
+      "hours_per_day": req.body["hours-per-day"],
+      "vacation_per_year": req.body["vacation-per-year"]
+    })
+
+    return res.redirect('/')
   }
 }
